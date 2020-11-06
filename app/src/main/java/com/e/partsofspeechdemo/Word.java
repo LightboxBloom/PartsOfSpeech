a37e7fdcc9bc23e00aa82cec7b045c38bc9c4524
Matthew Talbot <matthewsean.talbot@gmail.com>
2020-11-06 10:23:56 +0200

    Firebase added and offline mode implemented

---
+++ app/src/main/java/com/e/partsofspeechdemo/Word.java
@@ -0,0 +1,37 @@
+package com.e.partsofspeechdemo;
+
+import android.graphics.drawable.Drawable;
+
+/*
+* Class name: Word
+* Reason: This class is the Object class for each word
+* Parameters:
+* Team member: Matthew Talbot
+* Version 3:
+* 20/10/2020*/
+
+public class Word {
+    String text, partOfSpeech ;
+
+
+    public Word(String text, String partOfSpeech) {
+        this.text = text;
+        this.partOfSpeech = partOfSpeech;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getPartOfSpeech() {
+        return partOfSpeech;
+    }
+
+    public void setPartOfSpeech(String partOfSpeech) {
+        this.partOfSpeech = partOfSpeech;
+    }
+}
